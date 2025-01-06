439c0ef53a19737e7e985604dd3e66f1cf10d169
Tom Zubiri <tomzubiri@openscreensit.com>
2025-01-06 20:36:36 -0300

    refactored

--- AI1/play_ui.py
+++ AI1/play_ui.py
@@ -1,17 +1,21 @@
 import AI1
+import sys
+
+v = sys.argv[1]
+AI = AI1.play_1 if v == "1" else AI1.play_2
 
 def render_board(board):
-
     return board[:3] + '\n' + board[3:6] + '\n' + board[6:]
 
 board = '_________'
 print(render_board(board))
 
+
 while AI1.winner(board)=='':
     i = int(input('your move:'))
     board = AI1.make_move(board,i)
     print(render_board(board))
-    board = AI1.play_1(board)
+    board = AI1.play_2(board)
     print('ai\'s move:')
     print(render_board(board))
 
