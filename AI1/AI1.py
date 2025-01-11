2feb52abc49e83f63adc56641769f7fc49cb3a34
Tom Zubiri <tomzubiri@openscreensit.com>
2025-01-11 01:02:59 -0300

    no spaces, tabs

--- AI1/AI1.py
+++ AI1/AI1.py
@@ -3,40 +3,40 @@ PLAYER1_PIECE = 'o'
 PLAYER2_PIECE = 'x'
 
 def play_1(board):
-    chosen_move = legal_moves(board)[0]
-    board_after_move = make_move(board,chosen_move)
-    return board_after_move
+	chosen_move = legal_moves(board)[0]
+	board_after_move = make_move(board,chosen_move)
+	return board_after_move
 
 import random
 def play_2(board):
-    moves = legal_moves(board)
-    chosen_move = moves[random.randrange(len(moves))]
-    board_after_move = make_move(board,chosen_move)
-    return board_after_move
+	moves = legal_moves(board)
+	chosen_move = moves[random.randrange(len(moves))]
+	board_after_move = make_move(board,chosen_move)
+	return board_after_move
 
 def legal_moves(board):
-    legal_moves = []
-    for i,square in enumerate(board):
-        if square == EMPTY_PIECE:
-            legal_moves.append(i)
-    return legal_moves
+	legal_moves = []
+	for i,square in enumerate(board):
+		if square == EMPTY_PIECE:
+			legal_moves.append(i)
+	return legal_moves
 
 def turn(board):
-    # o is defined as the first to play
-    os = board.count(PLAYER1_PIECE)
-    xs = board.count(PLAYER2_PIECE)
+	# o is defined as the first to play
+	os = board.count(PLAYER1_PIECE)
+	xs = board.count(PLAYER2_PIECE)
 
-    if os == xs:
-        return PLAYER1_PIECE
-    elif os == xs+1:
-        return PLAYER2_PIECE
-    else:
-        raise Exception("Ilegal board state")
+	if os == xs:
+		return PLAYER1_PIECE
+	elif os == xs+1:
+		return PLAYER2_PIECE
+	else:
+		raise Exception("Ilegal board state")
 
 def make_move(board,square):
-    # this is actually just board[square] = turn(board)
-    board = board[:square] + turn(board) + board[square+1:]
-    return board
+	# this is actually just board[square] = turn(board)
+	board = board[:square] + turn(board) + board[square+1:]
+	return board
 
 def winner(board):
 	lines = [(0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6)]
--- AI1/play_ui.py
+++ AI1/play_ui.py
@@ -2,52 +2,52 @@ import AI1
 import sys
 
 if len(sys.argv) == 1:
-    raise Exception("Please specify who plays X")
+	raise Exception("Please specify who plays X")
 
 v = sys.argv[1]
 
 def human_move(board):
-    print(render_board(board))
-    i = input('your move:')
-    if len(i)==1:
-        return AI1.make_move(board,int(i))
-    elif len(i)==2:
-        x,y = 0,0
-        if i[0] in ("A","a"):
-            x=1
-        elif i[0] in ("B","b"):
-            x=2
-        elif i[0] in ("C","c"):
-            x=3
-        else:
-            raise Exception("Didn't understand move (E2)")
+	print(render_board(board))
+	i = input('your move:')
+	if len(i)==1:
+		return AI1.make_move(board,int(i))
+	elif len(i)==2:
+		x,y = 0,0
+		if i[0] in ("A","a"):
+			x=1
+		elif i[0] in ("B","b"):
+			x=2
+		elif i[0] in ("C","c"):
+			x=3
+		else:
+			raise Exception("Didn't understand move (E2)")
 
-        if i[1] in ("1","2","3"):
-            y = int(i[1])
-        else:
-            raise Exception("Didn't understand move (E3)")
+		if i[1] in ("1","2","3"):
+			y = int(i[1])
+		else:
+			raise Exception("Didn't understand move (E3)")
 
-        i = i.lower()
-        bytes(i,"ASCII")
-        return AI1.make_move(board,(x-1)*3+y-1)
-    else:
-        raise Exception("Didn't understand move (E1)")
-    return AI1.make_move(board,i)
+		i = i.lower()
+		bytes(i,"ASCII")
+		return AI1.make_move(board,(x-1)*3+y-1)
+	else:
+		raise Exception("Didn't understand move (E1)")
+	return AI1.make_move(board,i)
 
 
 
 if v == "1" or v =="default":
-    AI = AI1.play_1
+	AI = AI1.play_1
 elif v == "2" or v=="bobo" or v=="random":
-    AI = AI1.play_2
+	AI = AI1.play_2
 elif v == "0" or v=="none" or v=="" or v=="me" or v=="human":
-    AI = human_move
+	AI = human_move
 else:
-    raise Exception("unrecognized cli param")
+	raise Exception("unrecognized cli param")
 
 
 def render_board(board):
-    return board[:3] + '\n' + board[3:6] + '\n' + board[6:]
+	return board[:3] + '\n' + board[3:6] + '\n' + board[6:]
 
 board = '_________'
 
@@ -56,12 +56,12 @@ p2 = AI
 
 turn = "O"
 while AI1.winner(board)=='':
-    if turn =="O":
-        print("p1 turn")
-        board=p1(board)
-    elif turn =="X":
-        board= p2(board)
-    turn = "O" if turn == "X" else "X"
+	if turn =="O":
+		print("p1 turn")
+		board=p1(board)
+	elif turn =="X":
+		board= p2(board)
+	turn = "O" if turn == "X" else "X"
 
 
 
