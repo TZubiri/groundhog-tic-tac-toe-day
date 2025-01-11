2e74d589967633edacc31f5988cad9cf3bd2a8d5
Tom Zubiri <tomzubiri@openscreensit.com>
2025-01-10 23:55:14 -0300

    error when no params

--- AI1/play_ui.py
+++ AI1/play_ui.py
@@ -1,14 +1,24 @@
 import AI1
 import sys
 
+if len(sys.argv) == 1:
+	raise Exception("Please specify who plays X")
+
 v = sys.argv[1]
+
+def human_move(board):
+    print(render_board(board))
+    i = int(input('your move:'))
+    return AI1.make_move(board,i)
+
+
 
 if v == "1" or v =="default":
     AI = AI1.play_1
 elif v == "2" or v=="bobo" or v=="random":
     AI = AI1.play_2
 elif v == "0" or v=="none" or v=="" or v=="me" or v=="human":
-    AI = None
+    AI = human_move
 else:
     raise Exception("unrecognized cli param")
 
@@ -17,11 +27,6 @@ def render_board(board):
     return board[:3] + '\n' + board[3:6] + '\n' + board[6:]
 
 board = '_________'
-
-def human_move(board):
-    print(render_board(board))
-    i = int(input('your move:'))
-    return AI1.make_move(board,i)
 
 p1 = human_move
 p2 = AI
