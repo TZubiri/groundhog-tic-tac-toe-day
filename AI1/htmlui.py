5c37223468100e22889c7da4815a89c850a802a9
Tom Zubiri <tomzubiri@openscreensit.com>
2025-01-16 00:06:31 -0300

    removed flask. Fixed broken html

--- AI1/htmlui.py
+++ AI1/htmlui.py
@@ -1,13 +1,13 @@
-import flask
+import socketserver
 
-app = flask.Flask("app")
+
 
 htmlpre = "<html><form>"
 
 board = "_________"
 htmlsuf = "</form></html>"
 
-sq = "<div style='width=30%;height=30%;border=solid'></div>"
+sq = "<div style='width:30%;height:30%;border:solid'></div>"
 
 htmlboard = ""
 i=0
@@ -18,9 +18,23 @@ for square in board:
 
 html = htmlpre+htmlboard+htmlsuf
 
-@app.route("/ui")
 def ui():
 	return html
 
-app.run("0.0.0.0",8080)
+
+
+class echo(socketserver.BaseRequestHandler):
+	def handle(self):
+		l = len(html)
+		self.request.sendall(("HTTP1.0 200 \r\nContent-Length:"+str(l)+"\r\n\r\n"+html).encode("ASCII"))
+		self.request.close()
+server_address = ('127.0.0.1',8003)
+httpd = socketserver.TCPServer(server_address,echo)
+httpd.serve_forever()
+
+
+
+
+
+
  
